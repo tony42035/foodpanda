6c68a196d53a6b2f1f586ac84106ec24ef94ee04
Yu-hsun Chuang <tony42035@gmail.com>
2024-02-16 13:59:23 +0800

    更改API

--- src/App.vue
+++ src/App.vue
@@ -8,11 +8,11 @@
     <h2>餐廳列表</h2>
     <h5>共 {{ storeCount }} 間餐廳</h5>
     <div class="row">
-      <div class="col-md-4 g-0" v-for="store in allStores" :key="store.id">
+      <div class="col-md-4 g-0" v-for="(store, index) in allStores" :key="store.id">
         <div class="card p-2 m-2">
           <img :src="store.hero_listing_image" class="card-img-top" alt="...">
           <div class="card-body">
-            <h5 class="card-title">{{ store.name }}</h5>
+            <h5 class="card-title">{{ index+1 }}.{{ store.name }}</h5>
             <a :href="store.web_path" class="btn btn-primary">前往餐廳</a>
           </div>
         </div>
@@ -29,47 +29,19 @@ export default {
     return {
       allStores: {},
       storeCount: 0,
+      aipUrl: 'https://disco.deliveryhero.io/listing/api/v1/pandora/vendors?latitude=24.956594&longitude=121.4737595&language_id=6&country=tw&vertical=restaurants&configuration=Original&limit=48&offset=0&customer_type=regular',
     };
   },
   methods: {
       searchbtn() {
-        axios.post('https://disco.deliveryhero.io/search/api/v1/feed', {
-              "q": "",
-    "location": {
-        "point": {
-            "latitude": 24.956594,
-            "longitude": 121.4737595
-        }
-    },
-    "config": "Variant21",
-    "customer_id": "",
-    "vertical_types": [
-        "restaurants"
-    ],
-    "include_component_types": [
-        "vendors"
-    ],
-    "include_fields": [
-        "feed"
-    ],
-    "language_id": "6",
-    "opening_type": "delivery",
-    "platform": "web",
-    "session_id": "",
-    "language_code": "zh",
-    "customer_type": "regular",
-    "limit": 48,
-    "offset": 0,
-    "dps_session_id": "eyJzZXNzaX2lk......MjM1zOTl9",
-    "dynamic_pricing": 0,
-    "brand": "foodpanda",
-    "country_code": "tw",
-    "use_free_delivery_label": false
+        axios.get(this.aipUrl, {
+          headers: {
+            'x-disco-client-id' : 'web',
+          }
         })
         .then( res => {
-          this.allStores = res.data.feed.items[0].items;
-          this.storeCount = res.data.feed.count;
-          console.log(this.allStores);
+          this.allStores = res.data.data.items;
+          this.storeCount = res.data.data.available_count;
         })
       }
     }
