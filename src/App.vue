8c2007c07c96fcb4fd01eb418c78fd9daa941293
Yu-hsun Chuang <tony42035@gmail.com>
2024-02-16 14:23:04 +0800

    增加更新日期

--- src/App.vue
+++ src/App.vue
@@ -36,8 +36,7 @@ export default {
   },
   methods: {
       searchbtn() {
-        const now = new Date();
-        this.dateTime = now.toLocaleDateString();
+        this.updateDataTime();
 
         axios.get(this.aipUrl, {
           headers: {
@@ -51,6 +50,17 @@ export default {
         .catch( err => {
           console.log(err);
         })
+      },
+      updateDataTime() {
+        const now = new Date();
+        const year = now.getFullYear();
+        const month = String(now.getMonth() + 1).padStart(2, '0');
+        const day = String(now.getDate()).padStart(2, '0');
+        const hour = String(now.getHours()).padStart(2, '0');
+        const minute = String(now.getMinutes()).padStart(2, '0');
+        const second = String(now.getSeconds()).padStart(2, '0');
+
+        this.dateTime = `${year}-${month}-${day} ${hour}:${minute}:${second}`;
       }
     }
 }
