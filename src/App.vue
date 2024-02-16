d8740b99d137b3512560fea312d026c2b0917c90
Yu-hsun Chuang <tony42035@gmail.com>
2024-02-16 14:18:17 +0800

    更改LOGO

--- src/App.vue
+++ src/App.vue
@@ -7,12 +7,13 @@
   <div class="container" v-if="storeCount != 0">
     <h2>餐廳列表</h2>
     <h5>共 {{ storeCount }} 間餐廳</h5>
+    <p class="text-end">資料更新時間：{{ dateTime }}</p>
     <div class="row">
       <div class="col-md-4 g-0" v-for="(store, index) in allStores" :key="store.id">
         <div class="card p-2 m-2">
           <img :src="store.hero_listing_image" class="card-img-top" alt="...">
           <div class="card-body">
-            <h5 class="card-title">{{ index+1 }}.{{ store.name }}</h5>
+            <h5 class="card-title">{{ index+1 }} {{ store.name }}</h5>
             <a :href="store.web_path" class="btn btn-primary">前往餐廳</a>
           </div>
         </div>
@@ -30,10 +31,14 @@ export default {
       allStores: {},
       storeCount: 0,
       aipUrl: 'https://disco.deliveryhero.io/listing/api/v1/pandora/vendors?latitude=24.956594&longitude=121.4737595&language_id=6&country=tw&vertical=restaurants&configuration=Original&limit=48&offset=0&customer_type=regular',
+      dateTime: '',
     };
   },
   methods: {
       searchbtn() {
+        const now = new Date();
+        this.dateTime = now.toLocaleDateString();
+
         axios.get(this.aipUrl, {
           headers: {
             'x-disco-client-id' : 'web',
@@ -42,6 +47,9 @@ export default {
         .then( res => {
           this.allStores = res.data.data.items;
           this.storeCount = res.data.data.available_count;
+        })
+        .catch( err => {
+          console.log(err);
         })
       }
     }
